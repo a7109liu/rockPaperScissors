382e222af1cb4d0dc98389d068b8f570d7351f78
Alan <a7109liu@gmail.com>
2022-07-10 14:40:36 -0500

    Finished game

--- javascript.js
+++ javascript.js
@@ -4,8 +4,6 @@ let comp, player, compWins = 0, playerWins = 0;
 function computerPlay() {
     let randNum = Math.floor(Math.random()*100 + 1);
     
-    //console.log(randNum);
-
     if (randNum > 0 && randNum < 33) {
         comp = 'rock';
         return 'rock';
@@ -18,93 +16,48 @@ function computerPlay() {
         comp = 'scissors';
         return 'scissors';
     }
-
-    //console.log(compChoice);
 }
-
-// function playerPlay() {
-//     let playerChoice = prompt("Choose Rock, Paper, or Scissors!");
-//     let playerChoicelc = playerChoice.toLowerCase();
-//     //console.log(playerChoicelc);
-//     if (playerChoicelc == 'rock') {
-//         player = 'rock';
-//         return player;
-//     }
-//     else if (playerChoicelc == 'paper') {
-//         player = 'paper';
-//         return player;
-//     }
-//     else {
-//         player = 'scissors';
-//         return player;
-//     }
-    
-// }
 
 const lowerBody = document.querySelector('.lowerBody');
 const msg = document.createElement('p');
 msg.classList.add('msg');
+lowerBody.appendChild(msg);
 
 function playRound(playerSelection, computerSelection) {
     
-    // const lowerBody = document.querySelector('.lowerBody');
-    // const msg = document.createElement('p');
-    // msg.classList.add('msg');
-    
-    
     if (comp == player) {
         msg.textContent = "It is a tie!";
-
-        lowerBody.appendChild(msg);
-        //console.log('It is a tie!');
+        game();
     }
-    else if (comp == 'paper' && player == 'rock') {
-        
-        msg.textContent = 'The computer wins: Paper beats rock!';
-
-        lowerBody.appendChild(msg);
-        //console.log('The computer wins: Paper beats rock!');
+    else if (comp == 'paper' && player == 'rock') {    
+        msg.textContent = 'The computer won this round: Paper beats rock!';
         compWins += 1;
+        game();
     }
     else if (player == 'paper' && comp == 'rock') {
-        
-        msg.textContent = 'You win: Paper beats rock!';
-
-        //lowerBody.appendChild(msg);
-        //console.log('You win: Paper beats rock!');
+        msg.textContent = 'You won this round: Paper beats rock!';
         playerWins += 1;
+        game();
     }
-    else if (comp == 'rock' && player == 'scissors') {
-        
-        msg.textContent = 'The computer wins: Rock beats scissors!';
-
-        //lowerBody.appendChild(msg);
-        //console.log('The computer wins: Rock beats scissors!');
+    else if (comp == 'rock' && player == 'scissors') {    
+        msg.textContent = 'The computer won this round: Rock beats scissors!';
         compWins += 1;
+        game();
     }
-    else if (player == 'rock' && comp == 'scissors') {
-        
-        msg.textContent = 'You win: Rock beats scissors!';
-
-        //lowerBody.appendChild(msg);
-        //console.log('You win: Rock beats scissors!');
+    else if (player == 'rock' && comp == 'scissors') {       
+        msg.textContent = 'You won this round: Rock beats scissors!';
         playerWins += 1;
+        game();
     }
-    else if (comp == 'scissors' && player == 'paper') {
-        
-        msg.textContent = 'The computer wins: Scissor beats paper!';
-
-        //lowerBody.appendChild(msg);
-        //console.log('The computer wins: Scissosr beats paper!');
+    else if (comp == 'scissors' && player == 'paper') {  
+        msg.textContent = 'The computer won this round: Scissor beats paper!';
         compWins += 1;
+        game();
     }
     else if (player == 'scissors' && comp == 'paper') {
-        
-        msg.textContent = 'You win: Scissor beats paper!';
-
-        //lowerBody.appendChild(msg);
-        //console.log('You win: Scissors beats paper!');
+        msg.textContent = 'You won this round: Scissor beats paper!';
         playerWins += 1;
+        game();
     }
 }
 
@@ -112,6 +65,13 @@ const playerRock = document.querySelector('.rock');
 playerRock.addEventListener('click', () => {
     player = 'rock';
     playerRock.classList.add('playing');
+    
+    if (playerWins === 5 || compWins === 5) {
+        compWins = 0;
+        playerWins = 0;
+        whoWon.textContent = "";
+    }
+
     playRound(player, computerPlay());
 });
 
@@ -119,6 +79,13 @@ const playerPaper = document.querySelector('.paper');
 playerPaper.addEventListener('click', () => {
     player = 'paper';
     playerPaper.classList.add('playing');
+
+    if (playerWins === 5 || compWins === 5) {
+        compWins = 0;
+        playerWins = 0;
+        whoWon.textContent = "";
+    }
+
     playRound(player, computerPlay());
 });
 
@@ -126,6 +93,13 @@ const playerScissors = document.querySelector('.scissors');
 playerScissors.addEventListener('click', () => {
     player = 'scissors';
     playerScissors.classList.add('playing');
+
+    if (playerWins === 5 || compWins === 5) {
+        compWins = 0;
+        playerWins = 0;
+        whoWon.textContent = "";
+    }
+
     playRound(player, computerPlay());
 });
 
@@ -136,30 +110,32 @@ function removeTransition(e) {
 const btn = document.querySelectorAll('button');
 btn.forEach(button => button.addEventListener('transitionend', removeTransition));
 
+const winCount = document.querySelector('.winCount');
+const playerScore = document.createElement('div');
+playerScore.classList.add('playerWins');
+const compScore = document.createElement('div');
+compScore.classList.add('playerWins');
 
+const msgWin = document.querySelector('.msgWin');
+const whoWon = document.createElement('div');
+whoWon.classList.add('whoWon');
 
+winCount.appendChild(playerScore);
+winCount.appendChild(compScore);
+msgWin.appendChild(whoWon);
 
+function game() {
 
+    if (playerWins <= 5 || compWins <= 5) {
+        playerScore.textContent = `Player: ${playerWins}`;
+        compScore.textContent = `Computer: ${compWins}`;
 
-
-
-
-
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         playRound(playerPlay(), computerPlay());
-//         console.log(`  Computer: ${comp}`);
-//         console.log(`  Player: ${player}`);    
-//     }
+        if (playerWins == 5) {
+            whoWon.textContent = "You won the game!";
+        }
+        if (compWins == 5) {
+            whoWon.textContent = "The computer wins the game :(";
+        }
+    }
     
-//     console.log(`Computer's score: ${compWins}`);
-//     console.log(`Player's score: ${playerWins}`);
-// }
-
-// game();
-
-//playRound(playerPlay(), computerPlay());
-
-
-
-
+}
